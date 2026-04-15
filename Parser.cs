404186b050c53b9434b291d82ff8189aeeae82c7
Богдан Авдюков <maksnervy666@gmail.com>
2026-04-15 13:30:28 +0700

    added parser

--- Parser.cs
+++ Parser.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace WpfApp1
 {
-    internal class Parser
+    public class SyntaxError
     {
+        public string ErrorFragment { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public int Line { get; set; }
+        public int Column { get; set; }
     }
 }
